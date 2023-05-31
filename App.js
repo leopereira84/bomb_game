9be538913726178a4655b4cb250e6c6a9d7e4173
Leonardo Andrey Pereira <leonardo.pereira@itajai.sc.gov.br>
2023-05-31 10:50:53 -0300

    Finalizando a página de regras (estilo).

--- App.js
+++ App.js
@@ -1,6 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import Rules from './src/pages/Rules';
 import Start from './src/pages/Start';
 
 export default function App() {
-  return <Start />;
+  return (
+    <>
+      <Rules />
+      <StatusBar color="light" />
+    </>
+  );
 }
--- src/pages/Start/index.jsx
+++ src/pages/Start/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ButtonComponent from "../../components/Buttons";
 import { Container, Logo, Rules, SubTitle, Title } from "./styles";
 
-export default function Start (){
+export default function Start() {
   const handleNavToPlayAlone = () => {
     console.log("Teste de play alone");
   }
@@ -18,7 +18,7 @@ export default function Start (){
   return (
     <Container>
       <Logo source={require("../../assets/logoDark.png")} style={{ resizeMode: "contain" }} />
-      <Title>Bem-vindo ao {"\n"} Bomb Game</Title>
+      <Title>Bem-vindo ao {"\n"}Bomb Game</Title>
       <SubTitle>Escolha um modo de jogo.</SubTitle>
       <ButtonComponent buttonText={"Jogar Solo"} handlePress={handleNavToPlayAlone} />
       <ButtonComponent buttonText={"Jogar em Dupla"} handlePress={handleNavToPlayTogether} />
