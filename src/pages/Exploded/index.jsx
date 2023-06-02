e40ee7351bc5b43f9100c9e66c18a2ace6327e40
Leonardo Andrey Pereira <leonardo.pereira@itajai.sc.gov.br>
2023-06-02 12:05:11 -0300

    Finalizando página bomba explodiu, junto com navegação.

--- src/pages/Exploded/index.jsx
+++ src/pages/Exploded/index.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 import { Container, Logo, SucessImg, Title } from "./styled";
 
-import logoImg from "../../assets/logoDark.png";
-import sucessImg from "../../assets/bomba_cortada_matrix.png";
+import logoImg from "../../assets/logoLightRed.png";
+import sucessImg from "../../assets/bomba_explodiu.png";
 import ButtonComponent from "../../components/Buttons";
 import { useNavigation } from "@react-navigation/native";
 
-export default function Disarmed() {
+export default function Exploded() {
   const navigation = useNavigation();
 
   function handleNavToStart() {
-    navigation.navigate("Start");
+    navigation.navigate("Start");r
   }
 
   return (
     <Container>
       <Logo source={logoImg} style={{ resizeMode: 'contain' }} />
-      <Title>Parabéns!!!{"\n"}Você desarmou!</Title>
+      <Title>Você falhou, a{"\n"}bomba explodiu!!!</Title>
       <SucessImg source={sucessImg} style={{ resizeMode: 'contain' }} />
 
       <ButtonComponent buttonText="Página Inicial" handlePress={handleNavToStart} />
